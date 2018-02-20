39f21aa48843006136a746c6b5005a4d90bed9a7
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2018-02-20 16:07:09 +0530

    fibonacci!

--- mul/fib.asm
+++ mul/fib.asm
@@ -4,16 +4,20 @@ extern atoi
 SECTION .data
 forS: db '%s',10,0
 forD: db '%d',10,0
+one: db '1'
 SECTION .bss
 n RESB 64
-i RESB 64
 SECTION .text
 
 print1:
-mov rdi,forD
-mov [i],eax
-mov rsi,[i]
+push rcx
+push rdx
+mov rdi,forS
+mov rsi,one
+mov rax,0
 call printf
+pop rdx
+pop rcx
 jmp next
 
 global main
@@ -37,24 +41,28 @@ pop rcx
 mov rcx,1
 mov rdx,1
 
-mov rax,1
+mov rbx,1
 repeat:
-cmp rax,[n]
+cmp rbx,[n]
 jg end
-cmp rax,1
+cmp rbx,1
 jz print1
-cmp rax,2
+cmp rbx,2
 jz print1
-mov rbx, rcx
-add rbx,rdx
+mov rax, rcx
+add rax,rdx
 mov rcx,rdx
-mov rdx,rbx
-mov rsi, rbx
+mov rdx,rax
 mov rdi, forD
+mov rsi, rax
+mov rax,0
+push rcx
+push rdx
 call printf
-
+pop rdx
+pop rcx
 next:
-inc eax
+inc rbx
 jmp repeat
 
 end:
